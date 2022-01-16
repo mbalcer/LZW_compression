79dc437a6dc9ae3d640cf8d5147334e6408d9ee4
mbalcer <mateusz75319@gmail.com>
2022-01-16 12:13:02 +0100

    Added buttons handling

--- src/main/java/pl/edu/pbs/lzw_compression/controller/ViewController.java
+++ src/main/java/pl/edu/pbs/lzw_compression/controller/ViewController.java
@@ -1,14 +1,36 @@
 package pl.edu.pbs.lzw_compression.controller;
 
 import javafx.fxml.FXML;
-import javafx.scene.control.Label;
+import javafx.scene.control.TextArea;
+import pl.edu.pbs.lzw_compression.algorithm.LZW;
+
+import java.util.List;
+import java.util.stream.Collectors;
+import java.util.stream.Stream;
 
 public class ViewController {
     @FXML
-    private Label welcomeText;
+    private TextArea uncompressedTextLabel;
+    @FXML
+    private TextArea compressedTextLabel;
 
     @FXML
-    protected void onHelloButtonClick() {
-        welcomeText.setText("Welcome to JavaFX Application!");
+    protected void compressTextClick() {
+        String text = uncompressedTextLabel.getText();
+        List<Integer> compressed = LZW.compress(text);
+        String compressedString = compressed.stream()
+                .map(String::valueOf)
+                .collect(Collectors.joining(", "));
+        compressedTextLabel.setText(compressedString);
+    }
+
+    @FXML
+    protected void decompressTextClick() {
+        String text = compressedTextLabel.getText();
+        List<Integer> indexes = Stream.of(text.split(", "))
+                .map(Integer::parseInt)
+                .collect(Collectors.toList());
+        String decompressed = LZW.decompress(indexes);
+        uncompressedTextLabel.setText(decompressed);
     }
 }
